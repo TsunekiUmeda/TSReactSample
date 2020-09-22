fa8e62466ec06106a7b514f6a96d31416f534bfd
Tsuneki Umeda <umedat@amazon.co.jp>
2020-09-22 11:38:36 +0900

    Initializing state

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 interface AppProps {
-  color: string
+  color?: string
 }
-class App extends React.Component<AppProps> {
+
+interface AppState {
+  counter: number
+}
+
+class App extends React.Component<AppProps, AppState> {
+  //this work without AppState interface
+  // state = { counter: 0 }
+
+  constructor(props: AppProps) {
+    super(props)
+
+    //This wont work without second AppState interface
+    this.state = { counter: 0 }
+  }
+
+  onIncrement = () => {
+    this.setState({ counter: this.state.counter + 1 })
+  }
+  onDecrement = () => {
+    this.setState({ counter: this.state.counter - 1 })
+  }
   render() {
-    return <div>{this.props.color}</div>
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    )
   }
 }
-ReactDOM.render(<App color='red' />, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#root'))
